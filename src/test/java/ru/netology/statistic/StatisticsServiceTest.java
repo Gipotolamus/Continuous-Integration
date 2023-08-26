d9af304c0d7c23a21e5881260839f915d747976d
Mirau Pavel <miraupavel@gmail.com>
2023-08-26 15:54:11 +0700

    two commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxMidl() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 53, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 53;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
